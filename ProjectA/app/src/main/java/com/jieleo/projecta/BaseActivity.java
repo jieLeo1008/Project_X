951f1d1a5d1003c2f90f22af0adf0b3701c1ed52
砖头于老四 <753516036@qq.com>
2017-02-10 17:24:33 +0800

    BaseActivity

---
+++ ProjectA/app/src/main/java/com/jieleo/projecta/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.jieleo.projecta;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jie on 2017/2/10.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- ProjectA/app/src/main/java/com/jieleo/projecta/MainActivity.java
+++ ProjectA/app/src/main/java/com/jieleo/projecta/MainActivity.java
@@ -3,13 +3,7 @@ package com.jieleo.projecta;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //刘浩》》》》》》》》》
-        //sishuai
-    }
+
 }
